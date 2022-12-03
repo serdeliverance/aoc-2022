a0dbe660e0ec7e992707e8f9accff4315fd07158
Sergio Cano <canosergio90@gmail.com>
2022-12-03 17:48:42 +0100

    format
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- src/test/scala/io/github/sdev/aoc2022/day03/Day03Part2Spec.scala
+++ src/test/scala/io/github/sdev/aoc2022/day03/Day03Part2Spec.scala
@@ -3,18 +3,18 @@ package io.github.sdev.aoc2022.day03
 import io.github.sdev.aoc2022.day03.Day03Part2.solution
 
 class Day03Part2Spec extends munit.FunSuite {
-    test("determine badge and calculate priority sum") {
-        val input = List(
-            "vJrwpWtwJgWrhcsFMMfFFhFp",
-            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
-            "PmmdzqPrVvPwwTWBwg",
-            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
-            "ttgJtRGJQctTZtZT",
-            "CrZsJsPPZsGzwwsLwLmpwMDw"
-        )
+  test("determine badge and calculate priority sum") {
+    val input = List(
+      "vJrwpWtwJgWrhcsFMMfFFhFp",
+      "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+      "PmmdzqPrVvPwwTWBwg",
+      "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+      "ttgJtRGJQctTZtZT",
+      "CrZsJsPPZsGzwwsLwLmpwMDw"
+    )
 
-        val result = solution(input)
+    val result = solution(input)
 
-        assertEquals(result, 70)
-    }
+    assertEquals(result, 70)
+  }
 }
