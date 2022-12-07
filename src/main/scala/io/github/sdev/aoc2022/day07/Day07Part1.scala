3e7bf00ea43187372c759c00fb41022164f68766
Sergio Cano <canosergio90@gmail.com>
2022-12-07 22:23:10 +0100

    wip
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

---
+++ src/main/scala/io/github/sdev/aoc2022/day07/Day07Part1.scala
@@ -0,0 +1,59 @@
+package io.github.sdev.aoc2022.day07
+
+object Day07Part1:
+    import Parser._
+
+    def solution(input: List[String]) =
+        val fileSystem = parse(input)
+        fileSystem.root.calculateAllSizes().filter(_ < 100_000).sum
+
+    case class FileSystem(root: Root)
+
+    sealed trait FileSystemNode {
+        def ls(): Unit
+
+        def cd(directory: String): Option[Directory]
+
+        def size: Int
+    }
+    case class Root(content: List[FileSystemNode]) extends FileSystemNode {
+        def ls(): Unit = content.map(println)
+        def cd(directory: String): Option[Directory] = content.collect {
+            case d @ Directory(name, _ ) if name == directory => d
+        }.headOption
+
+        def size = content.map(_.size).sum
+        
+    }
+    case class Directory(name: String, content: List[FileSystemNode]) extends FileSystemNode {
+        def ls(): Unit = content.map(println)
+        def cd(directory: String): Option[Directory] = content.collect {
+            case d @ Directory(name, _ ) if name == directory => d
+        }.headOption
+
+        def size = content.map(_.size).sum
+    }
+
+    case class File(name: String, size: Int) extends FileSystemNode {
+        def ls(): Unit = println(s"size: $size name: $name")
+        def cd(directory: String): Option[Directory] = None
+    }
+
+    extension (root: Root)
+        def calculateAllSizes(): List[Int] =
+            root.content.map {
+                    case Directory(_, content) => ???
+                    case File(_, size) => size
+                    case _ => 0
+                
+            }
+
+    sealed trait Command
+    case object Ls extends Command
+    case class Cd(directory: String) extends Command
+
+object Parser:
+    import Day07Part1._
+
+    // TODO
+    def parse(input: List[String]): FileSystem = ???
---
+++ src/test/scala/io/github/sdev/aoc2022/day07/Day07Part1Spec.scala
@@ -0,0 +1,41 @@
+package io.github.sdev.aoc2022.day07
+
+import Day07Part1._
+
+class Day07Part1Spec extends munit.FunSuite {
+    
+    test("file size calculation") {
+        val input = File("file.txt", 2)
+
+        val result = input.size
+
+        assertEquals(result, 2)
+    }
+
+    test("directory with just files size calculation") {
+        val input = Directory("parent", List(File("file1.txt", 10)))
+
+        val result = input.size
+
+        assertEquals(result, 10)
+    }
+
+    test("directory with nested directory size calculation") {
+        
+        val input = Directory("parent", List(File("file1.txt", 10), Directory("nested", List(File("file2.txt", 10)))))
+
+        val result = input.size
+
+        assertEquals(result, 20)
+    }
+
+    test("root directory size calculation") {
+        
+        val directory = Directory("parent", List(File("file1.txt", 10), Directory("nested", List(File("file2.txt", 10)))))
+        val input = Root(List(File("bigFile.txt", 1000), directory))
+
+        val result = input.size
+
+        assertEquals(result, 1020)
+    }
+}
