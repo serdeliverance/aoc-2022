b7ea2c0ff78177a0b970ced1f8dd5c22da005294
Sergio Cano <canosergio90@gmail.com>
2022-12-07 22:44:55 +0100

    day 7 - not finished :( adding markers for future approaches (#1)
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- src/main/scala/io/github/sdev/aoc2022/day06/Day06Part2.scala
+++ src/main/scala/io/github/sdev/aoc2022/day06/Day06Part2.scala
@@ -4,8 +4,7 @@ import scala.collection.mutable.Queue
 import scala.annotation.tailrec
 import Day06Part1.findBeginningMessagePositionWithMarkerOfSize
 
-
 object Day06Part2:
 
-    def solution(input: String) =
-        findBeginningMessagePositionWithMarkerOfSize(input, 14)
+  def solution(input: String) =
+    findBeginningMessagePositionWithMarkerOfSize(input, 14)
--- src/test/scala/io/github/sdev/aoc2022/day06/Day06Part1Spec.scala
+++ src/test/scala/io/github/sdev/aoc2022/day06/Day06Part1Spec.scala
@@ -3,35 +3,35 @@ package io.github.sdev.aoc2022.day06
 import Day06Part1.solution
 
 class Day06Part1Spec extends munit.FunSuite {
-    test("find marker sequence #1") {
-        val input = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+  test("find marker sequence #1") {
+    val input = "bvwbjplbgvbhsrlpgdmjqwftvncz"
 
-        val result = solution(input)
+    val result = solution(input)
 
-        assertEquals(result, 5)
-    }
+    assertEquals(result, 5)
+  }
 
-    test("find marker sequence #2") {
-        val input = "nppdvjthqldpwncqszvftbrmjlhg"
+  test("find marker sequence #2") {
+    val input = "nppdvjthqldpwncqszvftbrmjlhg"
 
-        val result = solution(input)
+    val result = solution(input)
 
-        assertEquals(result, 6)
-    }
+    assertEquals(result, 6)
+  }
 
-    test("find marker sequence #3") {
-        val input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+  test("find marker sequence #3") {
+    val input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
 
-        val result = solution(input)
+    val result = solution(input)
 
-        assertEquals(result, 10)
-    }
+    assertEquals(result, 10)
+  }
 
-    test("find marker sequence #4") {
-        val input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+  test("find marker sequence #4") {
+    val input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
 
-        val result = solution(input)
+    val result = solution(input)
 
-        assertEquals(result, 11)
-    }
+    assertEquals(result, 11)
+  }
 }
